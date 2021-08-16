bc6bbb16d2581e66c525dee219d626249e2fe26f
Tamirawad <66210834+Tamirawad@users.noreply.github.com>
2021-08-16 19:52:04 +0300

    Add project files.

---
+++ ZribeStoreBeta1/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace ZribeStoreBeta1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Open-Source",
+            "Malware"});
+            this.listBox1.Location = new System.Drawing.Point(3, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(91, 342);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(97, 375);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Location = new System.Drawing.Point(103, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(142, 375);
+            this.panel2.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(3, 29);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(136, 342);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(263, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(265, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(265, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Release Date:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(562, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Catagory";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(42, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Apps";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(265, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Owner: COMPANY";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Purple;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button2.Location = new System.Drawing.Point(389, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 44);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Github";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 374);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "ZribeStore - BETA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ZribeStoreBeta1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZribeStoreBeta1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedItem.ToString() == "Open-Source")
+                {
+                    SetCatagory("opensource");
+                }
+                if (listBox1.SelectedItem.ToString() == "Malware")
+                {
+                    SetCatagory("malware");
+                }
+            } catch
+            {
+                // ss
+            }
+            
+        }
+
+
+
+
+        public void SetCatagory(string CA)
+        {
+            try
+            {
+                listBox2.Items.Clear();
+
+                if (CA == "opensource")
+                {
+                    listBox2.Items.Add("Notepad++");
+                    listBox2.Items.Add("7-ZIP");
+                    listBox2.Items.Add("VLC MediaPlayer");
+                    listBox2.Items.Add("Krita");
+                }
+
+                if (CA == "malware")
+                {
+                    listBox2.Items.Add("MalwareWatch");
+                    
+                }
+
+
+
+            }
+            catch
+            {
+                // ss
+            }
+
+
+
+
+        }
+
+        public void SetITEM(string item)
+        {
+            try
+            {
+
+
+                if (item == "notepad++")
+                {
+                    label1.Text = "Name: Notepad++";
+                    label2.Text = "Description: Amazing Open-Source Replacment For Windows Notepad!";
+                    label3.Text = "Release Date: UNKNOWN";
+                }
+
+                if (item == "vlcmp")
+                {
+                    label1.Text = "Name: VLC MediaPlayer";
+                    label2.Text = "Description: Great Open-Source MediaPlayer!";
+                    label3.Text = "Release Date: UNKNOWN";
+                }
+
+                if (item == "7zip")
+                {
+                    label1.Text = "Name: 7-ZIP";
+                    label2.Text = "Description: Open-Source Archive Application.";
+                    label3.Text = "Release Date: UNKNOWN";
+                }
+                if (item == "krita")
+                {
+                    label1.Text = "Name: Krita";
+                    label2.Text = "Description: Open-Source Art Application.";
+                    label3.Text = "Release Date: UNKNOWN";
+                }
+
+
+                if (item == "MalwareWatch")
+                {
+                    label1.Text = "Name: MalwareWatch";
+                    label2.Text = "Description: Malware And Application Database.";
+                    label3.Text = "Release Date: ?/?/2017";
+                    label6.Text = "Owner: Enderman - ON MALWAREWATCH / GITHUB";
+                }
+
+
+
+            }
+            catch
+            {
+                // ss
+            }
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox2.SelectedItem.ToString() == "Notepad++")
+                {
+                    SetITEM("notepad++");
+                }
+                if (listBox2.SelectedItem.ToString() == "VLC MediaPlayer")
+                {
+                    SetITEM("vlcmp");
+                }
+                if (listBox2.SelectedItem.ToString() == "7-ZIP")
+                {
+                    SetITEM("7zip");
+                }
+                if (listBox2.SelectedItem.ToString() == "Krita")
+                {
+                    SetITEM("krita");
+                }
+                if (listBox2.SelectedItem.ToString() == "MalwareWatch")
+                {
+                    SetITEM("MalwareWatch");
+                }
+            }
+            catch
+            {
+                // ss
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox2.SelectedItem.ToString() == "Notepad++")
+                {
+                    Process.Start("https://notepad-plus-plus.org/");
+                }
+
+                if (listBox2.SelectedItem.ToString() == "7-ZIP")
+                {
+                    Process.Start("https://www.7-zip.org/");
+                }
+
+                if (listBox2.SelectedItem.ToString() == "VLC MediaPlayer")
+                {
+                    Process.Start("https://www.videolan.org/vlc/index.html");
+                }
+                if (listBox2.SelectedItem.ToString() == "Krita")
+                {
+                    Process.Start("https://krita.org/");
+                }
+
+                if (listBox2.SelectedItem.ToString() == "MalwareWatch")
+                {
+                    Process.Start("https://malwarewatch.org/");
+                }
+            }
+            catch
+            {
+                // ss
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox2.SelectedItem.ToString() == "Notepad++")
+                {
+                    Process.Start("https://github.com/notepad-plus-plus/notepad-plus-plus");
+                }
+
+                if (listBox2.SelectedItem.ToString() == "7-ZIP")
+                {
+                    Process.Start("https://github.com/kornelski/7z");
+                }
+
+                if (listBox2.SelectedItem.ToString() == "VLC MediaPlayer")
+                {
+                    Process.Start("https://github.com/videolan/vlc");
+                }
+                if (listBox2.SelectedItem.ToString() == "Krita")
+                {
+                    Process.Start("https://github.com/KDE/krita");
+                }
+                if (listBox2.SelectedItem.ToString() == "MalwareWatch")
+                {
+                    Process.Start("https://github.com/Endermanch/MalwareDatabase");
+                }
+            }
+            catch
+            {
+                // ss
+            }
+        }
+    }
+}
